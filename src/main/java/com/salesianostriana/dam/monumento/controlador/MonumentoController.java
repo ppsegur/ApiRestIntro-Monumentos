8e82a98d502e174d6c8e2ef33415988697fd13f6
ppsegur <pepesegurarodriguez@gmail.com>
2024-10-02 20:54:38 +0200

    Servicio y controlador

---
+++ src/main/java/com/salesianostriana/dam/monumento/controlador/MonumentoController.java
@@ -0,0 +1,52 @@
+package com.salesianostriana.dam.monumento.controlador;
+
+
+
+
+
+
+import com.salesianostriana.dam.monumento.modelo.Monumento;
+import com.salesianostriana.dam.monumento.servicio.MonumentoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/monumentos")
+@RequiredArgsConstructor
+public class MonumentoController {
+
+
+    private final MonumentoService service;
+
+
+
+    @PostMapping("/crear")
+    public ResponseEntity<Monumento>crear(@RequestBody Monumento monumento) {
+        Monumento monumento1 = service.save(monumento);
+        return ResponseEntity.status(201).body(monumento1);/*201=creación del objeto correctamente*/
+    }
+
+
+    @GetMapping("/buscar/{id}")
+    public ResponseEntity<Monumento> buscar(@PathVariable Long id) {
+        Monumento monumento = service.findById(id);
+        return ResponseEntity.ok(monumento);
+    }
+
+
+    @PutMapping("/editar/{id}")
+    public ResponseEntity<Monumento> editar(@RequestBody Monumento monumento, @PathVariable Long id) {
+        Monumento monumento1 = service.edit(monumento, id);
+        return ResponseEntity.status(201).body(monumento1);
+    }
+
+
+    @DeleteMapping("/eliminar/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id) { /* el ? para devolver algo ya que el monumento ha sido borrado*/
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
+
